1c7c128f01f1dcd9352d0ea9613ed9708eeb42cf
YINCHEN-TING <yinchen777@hotmail.com>
2020-06-02 15:07:09 +0800

    Updates

---
+++ precache-manifest.a5ccbbb202599a0d8154c54a6fbb95f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bcf1a883eb2b9b3f9a799000f3e674",
+    "url": "/my-first-react/index.html"
+  },
+  {
+    "revision": "dae0adad2c9f1b13b0f4",
+    "url": "/my-first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ac8cc25562f1543a86a",
+    "url": "/my-first-react/static/js/2.ce997abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react/static/js/2.ce997abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae0adad2c9f1b13b0f4",
+    "url": "/my-first-react/static/js/main.677fda9c.chunk.js"
+  },
+  {
+    "revision": "dd98bdcc63c4433c4176",
+    "url": "/my-first-react/static/js/runtime-main.52a4880b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
